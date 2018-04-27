340be64ef5fd2181888e7cb65036d25b28cf237e
khyati0212 <khyati21136@gmail.com>
2018-04-27 14:12:48 -0700

    Adding some comments issue #3 issue #1

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+//returns name of the weekday based of the number
   getDayName(number) {
       let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
       return days[number];
     }
+    //returns name of the month  based of the number
   getMonthName(number) {
         let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
         return months[number];
       }
 
   render() {
-      let today = new Date().getDay();
-      let date = new Date().getDate();
-      let month = new Date().getMonth();
-      let year = new Date().getFullYear();
-      // If it is weekday
+      let today = new Date().getDay();//to get weekday number
+      let date = new Date().getDate();//to get date
+      let month = new Date().getMonth();//to get month
+      let year = new Date().getFullYear();//to get year
 
+      // If today is weekday --> name of day, month and date : MM/DD/YYYY will be printed
       if (today === 6 || today === 0)
           return <div>Today is {this.getDayName(today)} of Month : {this.getMonthName(month)}  date:{(month)}/ {(date)} / {(year)} - You can watch TV all day today</div>;
 
-      // If it is weeken
+      // If today is weekend --> name of day, month and date : MM/DD/YYYY will be printed
       return <div>Today is {this.getDayName(today)} of Month : {this.getMonthName(month)}  date:{(month)}/ {(date)} / {(year)}   - You should go to work today</div>;
 
   }
