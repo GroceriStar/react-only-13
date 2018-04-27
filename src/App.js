2a353afddf5d393a3a34d037f3c5053a8f1763a1
khyati0212 <khyati21136@gmail.com>
2018-04-27 10:37:26 -0700

    Master React, Quick Code tutorial. Part 2 issue #3 issue #1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+
+  getDayName(number) {
+      let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      return days[number];
+    }
+  getMonthName(number) {
+        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        return months[number];
+      }
+
   render() {
-    return (
-      <div>Hello World</div>
-    );
+      let today = new Date().getDay();
+      let date = new Date().getDate();
+      let month = new Date().getMonth();
+      let year = new Date().getFullYear();
+      // If it is weekday
+
+      if (today === 6 || today === 0)
+          return <div>Today is {this.getDayName(today)} of Month : {this.getMonthName(month)}  date:{(month)}/ {(date)} / {(year)} - You can watch TV all day today</div>;
+
+      // If it is weeken
+      return <div>Today is {this.getDayName(today)} of Month : {this.getMonthName(month)}  date:{(month)}/ {(date)} / {(year)}   - You should go to work today</div>;
+
   }
 }
 
