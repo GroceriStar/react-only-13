1f17f6ff7936b0e347c8fd64ccff4a476fe137f3
khyati0212 <khyati21136@gmail.com>
2018-06-17 18:21:49 -0700

    Stuck with error #14 #1

--- src/components/MealPlan.js
+++ src/components/MealPlan.js
@@ -1,27 +1,80 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 class MealPlan extends Component {
-  state = {
+  constructor(props) {
+    super(props)
+  this.state = {
     meal: '',
-    meals: []
-  };
+    img:'',
+    desc:'',
+    step:'',
+    meals: [],
+    descs: [],
+    steps: [],
+    imgs: []
+  }};
 renderMeals() {
-    return _.map(this.state.meals, meal => <li>{meal}</li>);
-  }
-  renderState(event){
-    this.setState({meal: event.target.value})
-  }
+    return _.map(
+
+    this.state.descs,desc=><li >{desc}</li>
+
+    )
+  };
+  renderMeal(e){
+    this.setState({meal:  e.target.value})
+  };
+  renderImg(e){
+    this.setState({img: e.target.value})
+  };
+  renderDesc(e){
+    this.setState({desc:  e.target.value})
+  };
+  renderStep({e}){
+    this.setState({step:  e.target.value})
+  };
 render() {
+
+  const onSubmit=e=>{ this.setState({meals: [...this.state.meals, this.state.meal.value],
+                                    imgs: [...this.state.imgs, this.state.img.value],
+                                    descs: [...this.state.descs, this.state.desc.value],
+                                    steps: [...this.state.steps, this.state.step.value]
+                                  })
+                      };
     return(
       <div>
+      <form >
         <h2>Today you should eat this</h2>
+
         <input
-          value={this.state.value}
-          onChange={this.renderState.bind(this)}
+          onChange={e => this.setState({meal: e.target.value})}
+          value={this.state.meal}
+          type="text"
+          required
+        />
+          <h2>Image: </h2>
+        <input
+          value={this.state.img}
+          onChange={e => this.setState({img: e.target.value})}
           type="text"
         />
+        <h2>Description: </h2>
+        <input
+          onChange={e => this.setState({desc: e.target.value})}
+          value={this.state.desc}
+          type="text"
+          required
+        />
+        <h2>Steps: </h2>
+        <input
+          onChange={e => this.setState({step: e.target.value})}
+          value={this.state.step}
+          type="text"
+          required
+        />
+        </form>
+
         <button
-          onClick={() => this.setState({meals: [...this.state.meals, this.state.meal]})}>
+      onClick={e=>onSubmit(e)}>
           Add meal</button>
         <ul>
           {this.renderMeals()}
